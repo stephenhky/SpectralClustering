76c894d807d336214c87b71ffc65ffa73721709f
stephenhky <stephenhky@yahoo.com.hk>
2020-05-15 12:18:56 -0400

    cmake readjusted

--- biowulf/setup.sh
+++ biowulf/setup.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+# making a folder for installing modules and modules
+cd $HOME
+if [ ! -f $HOME/installing ]; then
+  mkdir installing
+fi
+if [ ! -f $HOME/modules ]; then
+  mkdir modules
+fi
+cd installing
+
+# loading pre-existing packages
+#module load LAPACK/3.8.0/gcc-7.2.0-64    # loaded in python/3.7
+module load load python/3.7     # with openBLAS, LAPACK
+
 # installing cmake
-mkdir cmake
+if [ ! -f $HOME/installing/cmake ]; then
+  mkdir cmake
+fi
 cd cmake
 wget https://github.com/Kitware/CMake/releases/download/v3.17.2/cmake-3.17.2.tar.gz
 tar xvzf cmake-3.17.2.tar.gz
+mkdir $HOME/modules/cmake
 cd cmake-3.17.2/
-./configure --prefix=$HOME/cmake
+./configure --prefix=$HOME/modules/cmake
 ./bootstrap
 make
 make install
-export PATH=$HOME/cmake/cmake-3.17.2/bin:$PATH
+export PATH=$HOME/modules/cmake/bin:$PATH
 
 # installing ARPACK
 git clone https://github.com/yuj-umd/arpackpp.git
